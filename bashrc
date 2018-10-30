3a559fcfbd05a4d1e1589004738d66eaa868b8c3
Debian <bitnami@ip-172-31-53-225.ec2.internal>
2018-10-30 22:51:28 +0000

    More updates

--- bashrc
+++ bashrc
@@ -1,1 +1,14 @@
 #!/bin/bash
+PATH="~/bin:$PATH"
+export PATH
+unset LS_COLORS
+
+# Setup the environment
+JAVA_HOME=/opt/bitnami/java
+export JAVA_HOME
+OPENGROK_READ_XML_CONFIGURATION=/var/opengrok/etc/read-only.xml
+export OPENGROK_READ_XML_CONFIGURATION
+OPENGROK_DISTRIBUTION_BASE=/home/bitnami/opengrok/lib
+export OPENGROK_DISTRIBUTION_BASE
+OPENGROK_TOMCAT_BASE=/opt/bitnami/apache-tomcat
+export OPENGROK_TOMCAT_BASE
--- setup.sh
+++ setup.sh
@@ -1,1 +1,31 @@
-#!/bin/bash
+#!/bin/bash -xv
+
+# create the place for the commands
+if [ ! -d ~/bin ]; then
+	mkdir ~/bin
+fi
+# put the bash stuff in place.
+cp ~/frcopengrok/bashrc ~/.bash_frcopengrok
+cp ~/frcopengrok/bash_aliases ~/.bash_aliases
+# if it isn't already there, add the source.
+grep bash_frcopengrok ~/.bashrc
+if [ $? -ne 0 ]; then
+	echo '. ~/.bash_frcopengrok' >> ~/.bashrc
+fi
+exit
+sudo apt-get update
+sudo apt-get --yes install autotools-dev automake pkg-config mercurial subversion cvs cssc python3-pip
+# set the git editor
+git config --global core.editor vi
+# set up /var/opengrok
+if [ ! -d /var/opengrok ]; then
+	sudo mkdir /var/opengrok
+	sudo chown bitnami /var/opengrok
+fi
+# set up Bitnami Java as default
+sudo update-alternatives --install /usr/bin/java java /opt/bitnami/java/bin/java 100
+sudo update-alternatives --install /usr/bin/javac javac /opt/bitnami/java/bin/javac 100
+
+
+./getctags
+./getopengrok
